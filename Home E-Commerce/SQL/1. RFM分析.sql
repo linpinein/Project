a22e0244cd323f3775885387f89f169363cc0cee
linpinein <linpinein@#gmail.com>
2025-04-12 03:57:03 +0800

    第32~35行增加註解

--- Home E-Commerce/SQL/1. RFM分析.sql
+++ Home E-Commerce/SQL/1. RFM分析.sql
@@ -4,7 +4,7 @@
 	- 每位客戶在Orders表，所有訂單的消費總金額
 **************************************************************************************/
 CREATE VIEW 
-	RFM_Analyze as 																		-- 建立「RFM分析表」虛擬表，可供後面SQL查詢使用
+	RFM_Analyze as 																	-- 建立「RFM分析表」虛擬表，可供後面SQL查詢使用
 WITH F_M as (
 	SELECT DISTINCT
 		O.Customer_id,
@@ -26,18 +26,18 @@ WITH F_M as (
 	- 再算出 每位客戶 最近一次 Orders.order_purchase_timestamp 與 2023-08-30 天數差距
 **************************************************************************************/
 
-R as (																					-- 每個客戶最近一次消費日期（R）
+R as (																				-- 每個客戶最近一次消費日期（R）
 	SELECT DISTINCT
 		O.Customer_id,		
-		last_value(O.Order_purchase_timestamp) OVER (									-- last_value 找分區的最後一行
-			PARTITION by O.Customer_id
-			ORDER by O.Order_purchase_timestamp
-			ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING
+		last_value(O.Order_purchase_timestamp) OVER (								-- last_value 找分區的最後一行
+			PARTITION by O.Customer_id												-- 按 CustomerID 分區
+			ORDER by O.Order_purchase_timestamp										-- 以 購買時間 排序
+			ROWS BETWEEN CURRENT ROW AND UNBOUNDED FOLLOWING						-- 讓查詢從目前這行開始抓到分區最後一行，確保 last_value 抓到分區最後一筆資料
 		) as Last_time_purchase
 	FROM
 		Orders O
 )
-SELECT																					-- 天數差距 + 合併顯示F、M，存成 RFM_analyze.csv
+SELECT																				-- 天數差距 + 合併顯示F、M，存成 RFM_analyze.csv
 	R.Customer_id,
 	round(julianday('2023-08-30 00:00') - julianday(R.Last_time_purchase),2) as "Date_Diff(R)",		-- 沒有Datediff的替代品...
 	F_M."Purchase_Count(F)",
